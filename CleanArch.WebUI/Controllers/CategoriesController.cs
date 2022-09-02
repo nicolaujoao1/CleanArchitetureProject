5818f830ce7130c81eca841d66dad7ba385f1ef7
nicolaujoao1 <ilolanicolau1999@gmail.com>
2022-09-02 10:05:13 +0100

    Actualizando Categories

--- CleanArch.WebUI/Controllers/CategoriesController.cs
+++ CleanArch.WebUI/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.WebUI.Controllers
@@ -14,6 +15,32 @@ namespace CleanArch.WebUI.Controllers
         {
             var categories = await _categoryService.GetCategories();
             return View(categories);
+        }
+        [HttpGet()]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO categoryDto)
+        {
+            if (!ModelState.IsValid) return View();
+            await _categoryService.Add(categoryDto);
+            return RedirectToAction(nameof(Index));             
+        }
+        [HttpGet()]
+        public async Task<IActionResult>Edit(int id)
+        {
+            var categoryDto = await _categoryService.GetById(id);
+            if (categoryDto is null) return NotFound();
+            return View(categoryDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if (!ModelState.IsValid) return View(categoryDTO);
+            await _categoryService.Update(categoryDTO);
+            return RedirectToAction(nameof(Index));
         }
     }
 }

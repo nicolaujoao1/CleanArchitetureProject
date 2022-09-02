0e9c706d411c565c5c300ad099a18c0f0c2622be
nicolaujoao1 <ilolanicolau1999@gmail.com>
2022-09-02 08:29:54 +0100

    Trabalhos  Iniciais

--- CleanArch.WebUI/Program.cs
+++ CleanArch.WebUI/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute( 
     name: "default",
-    pattern: "{controller=Products}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
